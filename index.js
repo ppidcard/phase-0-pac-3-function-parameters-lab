8c53e0b722d96508e36362f0c4ab1a74a8f281f6
ppidcard <tvxqlikei@live.cn>
2021-03-24 22:04:53 +1100

    Done

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const name="Josh"
-function introduction(name){
-    return `Hi, my name is ${name}.`;
-  }
-  function introductionWithLanguage (name, language){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
 }
-function introductionWithLanguageOptional (name, language = "JavaScript"){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional (name, language="Script"){
+  
+  return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional (name, language="JavaScript"){
+  
+  return  `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
